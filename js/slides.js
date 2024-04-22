27136215221f87c9f5fa995789b109d386bca35b
rachaelmob <rachael.mo.b@gmail.com>
2024-04-22 17:36:08 +0100

    carousel

---
+++ js/slides.js
@@ -0,0 +1,18 @@
+//=== BANNER ===//
+$('.sliding').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    dots: true,
+});
+
+//=== BADGES ===/
+$('.badges-row').slick({
+    arrows: false,
+    swipe: false,
+    autoplay: true,
+    draggable: false,
+    autoplaySpeed: 3000,
+    variableWidth: true,
+});
+
