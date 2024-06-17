b8047ef97d42a50b616124687d23867b6f67f716
rachaelmob <rachael.mo.b@gmail.com>
2024-06-17 10:25:03 +0100

    Working on Database connection

---
+++ inc/dbconfig.php
@@ -0,0 +1,24 @@
+<?php 
+
+$dbconfig = parse_ini_file(__DIR . "/.env");
+
+$host = $env["DB_HOST"];
+$dbname = $env["DB_NAME"];
+$dbuser = $env["DB_USERNAME"];
+$password = $env["DB_PASSWORD"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
