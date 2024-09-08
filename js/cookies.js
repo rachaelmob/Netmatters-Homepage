0af4bb56e381a224bddd1a9726ed99087da9760b
rachaelmob <rachael.mo.b@gmail.com>
2024-09-08 15:05:53 +0100

    cookies pop up

--- js/cookies.js
+++ js/cookies.js
@@ -1,26 +1,55 @@
-const bodyHTML = document.querySelector('body')
 const manageConsent = document.querySelector('.manage-consent')
-const cookiesPopup = document.getElementById('cookies-consent')
+const cookiesId = document.getElementById('cookies-consent')
 const cookiesAccepted = document.querySelector('.cookies-accepted')
-
-
-// -- Cookies show up on window load 
-window.onload = function () {
-    // Only load IF cookies were accepted already
-    // if(localStorage.getitem("Accepted") === false ){
-        cookiesPopup.classList.toggle('cookie-popup');
-    // }
-
-};
-
-// -- IF cookies are accepted set "Accepted" in local storage
-
 
 
 // -- Cookies popup on manage button click
 manageConsent.addEventListener('click', function () {
-    cookiesPopup.classList.toggle('cookie-popup');
+    cookiesId.classList.toggle('cookie-popup');
 });
+
+
+// Function to create/set cookie
+setCookie = (cName, cValue, expDays) => {
+    let date = new Date();
+    // calculate date
+    date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    // create and save cookie at root
+    document.cookie = cName + "=" + cValue + ";" + expires + "; path=/";
+}
+
+// Function to check if cookie is available 
+getCookie = (cName) => {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie);
+    const cArr = cDecoded.split("; ");
+
+    let value;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) value = val.substring(name.length);
+    })
+
+    return value;
+}
+
+// Message disappeares when cookie is accepted
+cookiesAccepted.addEventListener("click", () => {
+    cookiesId.style.display = "none";
+
+    setCookie("cookieM", true, 365);
+})
+
+// check cookie
+
+cookieMessage = () => {
+    if (!getCookie("cookieM")) {
+        cookiesId.classList.toggle('cookie-popup');
+    } else {
+        localStorage.setItem("cookieM", true);
+    }
+}
+window.addEventListener("load", cookieMessage);
 
 
 
